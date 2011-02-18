77a94ed8dfce71a341adfa0674ed91c8af452cba
Christoph Redl <redl@kr.tuwien.ac.at>
2011-02-18 09:25:05 +0000

    DNA feature extractor: some small improvements

--- util/dnafeatureextractor/src/DNAFeatureExtractor.cpp
+++ util/dnafeatureextractor/src/DNAFeatureExtractor.cpp
@@ -35,32 +35,44 @@ using namespace dlvhex::dd::tools::dna;
 
 int main(int argc, char* argv[])
 {
+	string sequence;
+	vector<DNAFeature*> features;
+
+	// instanciate features
+	f1 _f1; f2 _f2; f3 _f3; f4 _f4; f5 _f5; f6 _f6; f7 _f7; f8 _f8; f9 _f9; f10 _f10; f11 _f11; f12 _f12; f13 _f13; f14 _f14; f15 _f15; f16 _f16; f17 _f17; f18 _f18; f19 _f19; f20 _f20; // f21 _f21; f22 _f22;
+	features.push_back(&_f1); features.push_back(&_f2); features.push_back(&_f3); features.push_back(&_f4); features.push_back(&_f5); features.push_back(&_f6); features.push_back(&_f7); features.push_back(&_f8); features.push_back(&_f9); features.push_back(&_f10); features.push_back(&_f11); features.push_back(&_f12); features.push_back(&_f13); features.push_back(&_f14); features.push_back(&_f15); features.push_back(&_f16); features.push_back(&_f17); features.push_back(&_f18); features.push_back(&_f19); features.push_back(&_f20); // features.push_back(&_f21); 
+
+	// use comma as decimal point
+	/*
+	struct lconv locale_structure;
+
+	struct lconv *locale_ptr=&locale_structure;
+
+	locale_ptr=localeconv();
+
+	*(locale_ptr->decimal_point) = ',';
+	*/
+
+
+	// Computation
 	if (argc < 2){
-		cout << "Filename expected!" << endl;
-		return 1;
+		// from standard in
+
+		while(std::getline(cin, sequence)){
+			cout << sequence;
+			for (vector<DNAFeature*>::iterator feature = features.begin(); feature != features.end(); feature++){
+				printf(",%f", (*feature)->extract(sequence));
+			}
+			cout << endl;
+		}
 	}else{
+		// from file
 		ifstream ifs(argv[1]);
-		string sequence;
-		vector<DNAFeature*> features;
 
-		// instanciate features
-		f1 _f1; f2 _f2; f3 _f3; f4 _f4; f5 _f5; f6 _f6; f7 _f7; f8 _f8; f9 _f9; f10 _f10; f11 _f11; f12 _f12; f13 _f13; f14 _f14; f15 _f15; f16 _f16; f17 _f17; f18 _f18; f19 _f19; f20 _f20; // f21 _f21; f22 _f22;
-		features.push_back(&_f1); features.push_back(&_f2); features.push_back(&_f3); features.push_back(&_f4); features.push_back(&_f5); features.push_back(&_f6); features.push_back(&_f7); features.push_back(&_f8); features.push_back(&_f9); features.push_back(&_f10); features.push_back(&_f11); features.push_back(&_f12); features.push_back(&_f13); features.push_back(&_f14); features.push_back(&_f15); features.push_back(&_f16); features.push_back(&_f17); features.push_back(&_f18); features.push_back(&_f19); features.push_back(&_f20); // features.push_back(&_f21); 
-
-		// use comma as decimal point
-		/*
-		struct lconv locale_structure;
-		struct lconv *locale_ptr=&locale_structure;
-
-		locale_ptr=localeconv();
-		*(locale_ptr->decimal_point) = ',';
-		*/
-
-		// compute features
 		while(std::getline(ifs, sequence)){
 			cout << sequence;
 			for (vector<DNAFeature*>::iterator feature = features.begin(); feature != features.end(); feature++){
-				printf(" %f", (*feature)->extract(sequence));
+				printf(",%f", (*feature)->extract(sequence));
 			}
 			cout << endl;
 		}
